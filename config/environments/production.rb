620c675e00fb2299c091bc16273fe973e57c0ca6
Mário Marroquim <mariomarroquim@ufpi.edu.br>
2026-05-09 07:55:36 -0300

    Update Rails version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Log requests in a single line.
   config.lograge.enabled = true
   config.lograge.custom_payload do |controller|
-    { current_user_id: Current.user.try(:id) }
+    { current_user_id: controller.current_user&.id }
   end
 
   # Settings specified here will take precedence over those in config/application.rb.
@@ -31,20 +31,20 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
-  config.assume_ssl = true
+  # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # config.force_ssl = true
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
 
   # Log to STDOUT with the current request id as a default log tag.
   config.log_tags = [ :request_id ]
-  config.logger = ActiveSupport::TaggedLogging.logger(STDOUT)
+  config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
 
-  # Change to "debug" to log everything (including potentially personally-identifiable information!)
-  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "error")
+  # Change to "debug" to log everything (including potentially personally-identifiable information!).
+  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
   # Prevent health checks from clogging up the logs.
   config.silence_healthcheck_path = "/up"
@@ -53,20 +53,19 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
 
   # Replace the default in-process memory cache store with a durable alternative.
-  config.cache_store = :solid_cache_store
+  # config.cache_store = :mem_cache_store
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
-  config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
+  # config.active_job.queue_adapter = :resque
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "example.com" }
 
-  # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
+  # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
   # config.action_mailer.smtp_settings = {
   #   user_name: Rails.application.credentials.dig(:smtp, :user_name),
   #   password: Rails.application.credentials.dig(:smtp, :password),
--- db/schema.rb
+++ db/schema.rb
@@ -10,56 +10,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_24_194413) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_09_103913) do
   create_table "sessions", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "ip_address"
-    t.string "user_agent"
     t.datetime "created_at", null: false
+    t.string "ip_address"
     t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
   create_table "test_plans", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "feature", null: false
-    t.string "use_case", null: false
-    t.text "steps_to_reproduce", null: false
     t.datetime "created_at", null: false
+    t.string "feature", null: false
+    t.text "steps_to_reproduce", null: false
     t.datetime "updated_at", null: false
+    t.string "use_case", null: false
+    t.integer "user_id", null: false
     t.index ["feature", "use_case"], name: "index_test_plans_feature_use_case", unique: true
     t.index ["user_id"], name: "index_test_plans_on_user_id"
   end
 
   create_table "test_runs", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "test_plan_id", null: false
-    t.string "revision", null: false
-    t.boolean "passed", null: false
-    t.text "observations"
     t.datetime "created_at", null: false
+    t.text "observations"
+    t.boolean "passed", null: false
+    t.string "revision", null: false
+    t.integer "test_plan_id", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["test_plan_id"], name: "index_test_runs_on_test_plan_id"
     t.index ["user_id"], name: "index_test_runs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email_address", null: false
-    t.string "password_digest", null: false
-    t.date "paid_at"
     t.boolean "admin", default: false, null: false
     t.datetime "created_at", null: false
+    t.string "email_address", null: false
+    t.date "paid_at"
+    t.string "password_digest", null: false
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
   create_table "versions", force: :cascade do |t|
-    t.string "whodunnit"
     t.datetime "created_at"
+    t.string "event", null: false
     t.bigint "item_id", null: false
     t.string "item_type", null: false
-    t.string "event", null: false
     t.text "object", limit: 1073741823
+    t.string "whodunnit"
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
